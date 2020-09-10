671fd323fb6529d3186cc30a9a4607e8e101a5e3
warinthon30 <jaitrong3016@gmail.com>
2020-09-10 09:52:40 +0700

    commit1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {5+3} </h1>
+      <h3>Hello {"6135512053"} </h3>
+    </div>
+  );
+}
+
+export default App;
