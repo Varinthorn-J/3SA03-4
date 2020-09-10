2050533a7ca0be0af583ff3907788f81853356c8
warinthon30 <jaitrong3016@gmail.com>
2020-09-10 10:53:29 +0700

    commit8

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 export default function WordCard(props){
- return (
- <div>
-{ Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
-}
+    return (
+        <div>
+        <WordCard value="hello"/>
+        </div>
+        );
+    }       
