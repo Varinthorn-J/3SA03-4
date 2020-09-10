8b8ff55daa971211928b584f9a19ae1cb0be0bc4
warinthon30 <jaitrong3016@gmail.com>
2020-09-10 11:08:29 +0700

    commit9

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 export default function WordCard(props){
+    const activationHandler = c => { console.log(`${c} has been activated.`) }
     return (
         <div>
-        <WordCard value="hello"/>
+       <CharacterCard value={c} key={i} activationHandler={activationHandler}/>
         </div>
         );
     }       
